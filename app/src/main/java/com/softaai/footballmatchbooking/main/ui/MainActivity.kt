124b78c819c51de24a9decce9750986a6c532c19
amol pawar <amoljp19@gmail.com>
2021-01-31 23:32:24 +0530

    can not resolved hilt dependency issue solved

--- app/src/main/java/com/softaai/footballmatchbooking/main/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/footballmatchbooking/main/ui/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.softaai.footballmatchbooking.main.ui
 
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.liveData
-import com.softaai.footballmatchbooking.data.model.allevents.AllEventsResponse
-import com.softaai.footballmatchbooking.utils.Resource
+import androidx.lifecycle.Observer
+import com.softaai.footballmatchbooking.R
+import com.softaai.footballmatchbooking.main.viewmodel.AllEventsViewModel
+import com.softaai.footballmatchbooking.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val allEventsViewModel : AllEventsViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        allEventsViewModel.allEvents.observe(this, Observer {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    it.data?.let { allEvent -> Toast.makeText(this, "" + allEvent, Toast.LENGTH_LONG).show() }
+                }
+                Status.LOADING -> {
+
+                }
+                Status.ERROR -> {
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
 }
